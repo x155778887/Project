73bbcc60f5b3beffb1b59cf360f4e5f0d329f392
Megu <jovi10161@gmail.com>
2016-12-10 17:12:03 +0000

    Added WiP Form

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,19 @@
+var captchaInput = document.getElementById("captchaInput");
+function weed() {
+    if (captchaInput === "Earth") {
+        window.alert("Sign up successful!");
+    } else {
+        window.alert("You failed the captcha.");
+    }
+}
+
+
 // Navigator Functions
 navOpen = 0;
-activeDiv = 0;
 function navBar() {
     if (navOpen === 0) {
-        document.getElementById("mySidenav").style.width = "9%";
-        document.getElementById("main").style.marginLeft = "9%";
+        document.getElementById("mySidenav").style.width = "10%";
+        document.getElementById("main").style.marginLeft = "10%";
         document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
         navOpen = 1;
     } else {
